f179181641500a24997cd10b68ad85935ea29846
mielecarmine <mielecarmine993@gmail.com>
2024-01-12 15:33:03 +0100

    feat: dice game

--- Gioco dei dadi/javascript/main.js
+++ Gioco dei dadi/javascript/main.js
@@ -8,10 +8,12 @@
 //   - Pareggio e stampo l'avviso
 
 const randomUserNum = Math.round(Math.random() * 5) + 1;
+alert("L'utente ha il numero " + randomUserNum);
 const randomUserPc = Math.round(Math.random() * 5) + 1;
+alert("Il computer ha il numero " + randomUserPc);
 
 if (randomUserNum > randomUserPc) {
   alert("L'utente vince!");
 } else if (randomUserNum < randomUserPc) {
   alert("Il computer vince!");
-}
+} else alert("Pareggio");
